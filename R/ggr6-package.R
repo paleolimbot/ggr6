0e99d295442cd3b1e19cc7dbf6b161c9381dec59
Dewey Dunnington <dewey@fishandwhistle.net>
2019-07-05 21:21:42 -0400

    use R6

--- R/ggr6-package.R
+++ R/ggr6-package.R
@@ -1,8 +1,10 @@
+
 #' @keywords internal
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom R6 R6Class is.R6 is.R6Class
 ## usethis namespace: end
 NULL
