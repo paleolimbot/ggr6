4afc427eaa7284a82b15333e3691c3a301a3c6cc
Dewey Dunnington <dewey@fishandwhistle.net>
2019-07-15 11:12:00 -0400

    add slightly better interface for adding layers to a graphic

--- R/graphic.R
+++ R/graphic.R
@@ -20,6 +20,32 @@ Graphic <- R6Class(
       self$set_facet(FacetNull$new())
     },
 
+    add_layer = function(layer) {
+      self$layers$add(layer)
+      invisible(self)
+    },
+
+    add_scale = function(scale) {
+      existing_scales <- self$scales$filter_by_aesthetics(scale$aesthetics)
+      if (existing_scales$size() > 0) {
+        abort(
+          sprintf(
+            "Scale for aesthetic %s already exists.",
+            paste0("'", scale$aesthetics, "'", collapse = " / ")
+          )
+        )
+      }
+
+      self$scales$add(scale)
+      invisible(self)
+    },
+
+    replace_scale = function(scale) {
+      self$scales <- self$scales$discard_by_aesthetics(scale$aesthetics)
+      self$add_scale(scale)
+      invisible(self)
+    },
+
     set_coord = function(coord) {
       assert_r6(coord, "Coord")
       self$coord <- coord
--- tests/testthat/test-graphic.R
+++ tests/testthat/test-graphic.R
@@ -2,3 +2,23 @@
 test_that("Graphic objects can be initiated without arguments", {
   expect_true(is.R6(Graphic$new()))
 })
+
+test_that("Layer objects be be added using add_layer()", {
+  graphic <- Graphic$new()$add_layer(Layer$new())
+  expect_equal(graphic$layers$size(), 1)
+  expect_error(graphic$add_layer(NULL), class = "bad_r6_type")
+})
+
+test_that("Scale objects be be added using add_scale()", {
+  graphic <- Graphic$new()$add_scale(ScaleNull$new())
+  expect_equal(graphic$scales$size(), 1)
+  expect_error(graphic$add_scale(NULL), class = "bad_r6_type")
+})
+
+test_that("Graphic$add_scale() does not replace existing scales", {
+  graphic <- Graphic$new()$add_scale(ScaleNull$new("x"))
+  scale2 <- ScaleNull$new(c("x", "x2"))
+  expect_error(graphic$add_scale(scale2), "already exists")
+  expect_silent(graphic$replace_scale(scale2))
+  expect_identical(graphic$scales$scale("x"), scale2)
+})
